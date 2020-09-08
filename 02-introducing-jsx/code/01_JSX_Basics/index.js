ae311601efbf85c0c259a5b44de36f2999ebf258
dazacher <dazacher2000@yahoo.com>
2020-09-08 13:28:46 -0700

    Intro to JSX and how to embed JavaScript into it.

--- 02-introducing-jsx/code/01_JSX_Basics/index.js
+++ 02-introducing-jsx/code/01_JSX_Basics/index.js
@@ -1,5 +1,5 @@
 function getMood() {
-	const moods = [ 'Angry', 'Hungry', 'Silly', 'Quiet', 'Paranoid' ];
+	const moods = ['Angry', 'Hungry', 'Silly', 'Quiet', 'Paranoid'];
 	return moods[Math.floor(Math.random() * moods.length)];
 }
 class JSXDemo extends React.Component {
@@ -7,6 +7,8 @@ class JSXDemo extends React.Component {
 		return (
 			<div>
 				<h1>My Current Mood is: {getMood()}</h1>
+				<h1>My number is: {2 * 8.4}</h1>
+				<img src="https://images.unsplash.com/photo-1599557060314-ad81e896cf77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1559&q=80"></img>
 			</div>
 		);
 	}
